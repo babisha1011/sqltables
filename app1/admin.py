242b5f8c74aa66b045e699c8b29e6216391124c8
babisha1011 <babishasweety@gmail.com>
2023-12-08 18:26:06 +0530

    first commit

---
+++ app1/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from app1.models import *
+
+# Register your models here.
+admin.site.register(Dept)
+admin.site.register(Emp)
+admin.site.register(SalGrade)
+admin.site.register(Bonus)
